48688d2bccea51c7b2453dbfe637dc4463a4e8d6
Rahul Singh Shekhawat <91182435+Hrshw@users.noreply.github.com>
2023-05-07 13:10:05 +0530

    Add files via upload

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,16 +1,4 @@
-// const validateReferralCodeOnClient = async (referralCode) => {
-//   try {
-//     const response = await fetch(`/api/validate-referral-code?code=${referralCode}`);
-//     const data = await response.json();
-//     return data.valid; // assuming the response contains a 'valid' property
-//   } catch (error) {
-//     console.error(error);
-//     return false;
-//   }
-// };
-
- 
- document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('form');
     const username = document.getElementById('username');
     const email = document.getElementById('email');
@@ -53,7 +41,7 @@
         return re.test(phone);
       };
       
-    const validateInputs = () => {
+    const validateInputs = async () => {
         const usernameValue = username.value.trim();
         const emailValue = email.value.trim();
         const passwordValue = password.value.trim();
@@ -101,44 +89,7 @@
             setSuccess(password2);
           }
       
-          // if (otpValue === '') {
-          //   setError(otp, 'Please enter your otp');
-          // } else if (otpValue.length !== 6) {
-          //   setError(otp, 'Please enter a valid 6 digit OTP');
-          // } else {
-          //   const phoneNumber = phoneValue; // Replace with actual phone number
-          //   const response = await fetch('/verifyotp', {
-          //     method: 'POST',
-          //     headers: {
-          //       'Content-Type': 'application/json'
-          //     },
-          //     body: JSON.stringify({
-          //       phoneNumber,
-          //       otp: otpValue
-          //     })
-          //   });
-          //   const data = await response.json();
-          //   if (data.success) {
-          //     setSuccess(otp);
-          //   } else {
-          //     setError(otp, 'Invalid OTP');
-          //   }
-          }
-        //   if (referralNumberValue !== '') {
-        //     // Call referral code validation function
-        //     const isValidReferralCode = await validateReferralCodeOnClient(referralNumberValue);
-        //     if (isValidReferralCode) {
-        //       setSuccess(referralnumber);
-        //     } else {
-        //       setError(referralnumber, 'Invalid referral code');
-        //     }
-        //   } else {
-        //     setSuccess(referralnumber);
-        //   }
-        // } 
-        // catch (error) {
-        //   setError(referralnumber, error.message);
-        // }
+        }
 
           form.submit();
       };
